09e0bfa5d2df9510299e17cdf2db00fd9abc4fa3
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-01-03 20:26:56 +0300

    добавил вызов статистики животного

--- domain/farm.py
+++ domain/farm.py
@@ -3,12 +3,15 @@ class Farm:
     def __init__(self, n):
         self.__animals = []
 
+        for i in range(n):
+            self.__animals.append(Animal())
 
 
     def get_animal(self, n):
-        raise NotImplementedError
+        if n > 0 and n < len(self.__animals):
+            return self.__animals
 
-    def get_stats(self):
+    def get_stats(self, n):
         """
         [
             {
@@ -20,4 +23,13 @@ class Farm:
         ]
         :return:
         """
+        for j in range(n):
+            var = [
+                {
+                    'name': '',
+                    'hunger': '',
+                    'healf': '',
+                    'mood': '',
+                }
+            ]
         raise NotImplementedError
