49d6f85d1b5f6c2af5d5a7bf93443103e683c3d1
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-01-03 21:18:41 +0300

    обновил графику

--- src/domain/graphics.py
+++ src/domain/graphics.py
@@ -1,12 +1,18 @@
 import tkinter
 from tkinter import *
 
-window = Tk()
-window.title('Tamagotchi')
 
-button = Button(window, text="Начать игру", width=12, height=2, )
+farm = Tk()
+farm.title('Tamagotchi')
 
-button.pack()
+def open_animal_state():
+    state = Tk()
+    state.title('Animal Information')
+
+
+start = Button(farm, text="Начать игру", width=12, height=2,).pack()
+animal = Button(farm, width=12, height=2, command=open_animal_state).pack()
+
 
 
 mainloop()
