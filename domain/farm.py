fb1663893eb448fce8a079945d77e79ba7611eb8
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-01-03 19:44:28 +0300

    добавили формат статистики

--- domain/farm.py
+++ domain/farm.py
@@ -1,9 +1,20 @@
 class Farm:
-    def __init__(self):
+    def __init__(self, ):
         self.animals = []
 
     def get_animal(self, n):
         raise NotImplementedError
 
     def get_stats(self):
+        """
+        [
+            {
+                "name": "Natasha",
+                "health": 100,
+                "mood": 100,
+                "hunger": 100
+            }
+        ]
+        :return:
+        """
         raise NotImplementedError
