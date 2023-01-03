3727dcdc98b7bbdbcbd8133ed0412990c458f672
krekerysu <dimasidorovsciy@gmail.com>
2023-01-03 20:58:45 +0300

    добавили end и start

--- src/domain/game.py
+++ src/domain/game.py
@@ -1,6 +1,5 @@
 from threading import Thread
-from time import time
-
+import time
 from domain.farm import Farm
 
 
@@ -17,17 +16,16 @@ class Game:
         while True:
             if self.__started:
                 for animal in self.__farm.animals:
-                    # TODO: Tick every animal
-                    def __tick(self):
-                        self.__mood -= 1
-                        self.__hunger -= 1
-                        self.__health -= 1
 
+                    if not animal.is_alive():
+                        self.end()
+                        break
 
-
-
+                    animal.tick()
+            time.sleep(1)
 
     def start(self):
+        self.__started = True
         self.__farm = Farm(10)
         self.__points = 0
 
@@ -35,7 +33,12 @@ class Game:
     def points(self):
         return self.__points
 
+    @property
+    def record(self):
+        return self.__record
+
     def end(self):
+        self.__started = False
         if not self.__record or self.__points > self.__record:
             print("Вау, это новый рекорд:", self.__points)
             self.__record = self.__points
