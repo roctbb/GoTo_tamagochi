86d3a1d9867c9d0c169c84988f29da649406f75b
Gass <sashapapa8@gmail.com>
2023-01-03 22:22:58 +0300

    добавили папку gui

--- src/gui/mode_window.py
+++ src/gui/mode_window.py
@@ -7,28 +7,30 @@ from gui.farm_window import FarmWindow
 class ModeWindow:
     def __init__(self, game):
         self.game = game
-        window = Tk()
-        window.title("Выбор сложности")
-
-        def close_window():
-            quit()
+        self.window = Tk()
+        self.window.title("Выбор сложности")
 
         if game.record:
-            Label(window, text=game.record, width=25, height=5).pack()
+            Label(self.window, text=game.record, width=25, height=5).pack()
         else:
-            Label(window, text="Вы ещё не играли.", width=25, height=5).pack()
+            Label(self.window, text="Вы ещё не играли.", width=25, height=5).pack()
 
-        Button(window, text="Легко", width=30, height=5, command=self.__start_easy).pack()
-        Button(window, text="Средне", width=30, height=5, command=self.__start_medium).pack()
-        Button(window, text="Сложно", width=30, height=5, command=self.__start_hard ).pack()
+        Button(self.window, text="Легко", width=30, height=5, command=self.__start_easy).pack()
+        Button(self.window, text="Средне", width=30, height=5, command=self.__start_medium).pack()
+        Button(self.window, text="Сложно", width=30, height=5, command=self.__start_hard ).pack()
+
 
 
     def __start_easy(self):
         self.__start_game_with_difficulty(5)
+        self.window.destroy()
+
     def __start_medium(self):
         self.__start_game_with_difficulty(10)
+        self.window.destroy()
     def __start_hard(self):
         self.__start_game_with_difficulty(20)
+        self.window.destroy()
 
     def __start_game_with_difficulty(self, difficulty):
         self.game.start(difficulty)
