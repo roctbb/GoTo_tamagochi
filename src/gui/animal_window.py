34796374cfbc88dd52ef89c9c2ee8657b4777bbf
krekerysu <dimasidorovsciy@gmail.com>
2023-01-03 22:04:55 +0300

    Делаю окно для животного

--- src/gui/animal_window.py
+++ src/gui/animal_window.py
@@ -1,4 +1,3 @@
-
 from tkinter.ttk import *
 from tkinter import *
 from tkinter import messagebox
@@ -8,38 +7,30 @@ from domain.animal import Animal
 
 class AnimalWindow:
     def __init__(self, animal):
-        farm = Tk()
-        farm.title('Tamagotchi')
-def check_life():
-    current_life = life.get()
+        self.window = Tk()
+        self.window.title("Питомец")
+        self.health = IntVar()
+        self.health.set(animal.health)
+        self.animal = animal
 
-    life.set(current_life - 1)
+        Progressbar(self.window, variable=self.health).pack(padx=25, pady=25)
+        Button(self.window, text="Покормить", command=self.feed).pack(padx=25, pady=25)
+        Progressbar(self.window, variable=self.health).pack(padx=25, pady=25)
+        Button(self.window, text="Поиграть", command=self.play).pack(padx=25, pady=25)
+        self.window.after(1000, self.update)
 
-    if current_life <= 1:
-        messagebox.showerror(title="Вы проиграли!", message="Зверек умер... :(")
-        quit()
-    else:
-        window.after(1000, check_life)
+    def update(self):
+        self.health.set(self.animal.health)
+        self.window.after(1000, self.update)
 
-def feed():
-    life.set(100)
+    def feed(self):
+        self.animal.feed()
 
-window = Tk()
-window.title("Питомец")
+    def play(self):
+        self.animal.play()
 
-life = IntVar()
-life.set(50)
-
-Progressbar(window, variable=life).pack(padx=25, pady=25)
-
-Button(window, text="Покормить", command=feed).pack(padx=25, pady=25)
-
-window.after(1000, check_life)
-
-mainloop()
 
 if __name__ == "__main__":
     animal = Animal()
     AnimalWindow(animal)
     mainloop()
-
