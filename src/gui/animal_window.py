8dcba6e2bb090c0c06d37f0c9d36ee610b16b208
krekerysu <dimasidorovsciy@gmail.com>
2023-01-04 14:16:06 +0300

    Еще довожу до идеала 3.0

--- src/gui/animal_window.py
+++ src/gui/animal_window.py
@@ -2,6 +2,8 @@ from tkinter.ttk import *
 from tkinter import *
 from tokenize import Name
 
+from pkg_resources.extern import names
+
 from domain.game import Game
 
 number = 0
@@ -33,7 +35,7 @@ class AnimalWindow:
         Progressbar(self.window, variable=self.mood_var, maximum=animal.pr_max_mood).grid(row=3, column=2)
         Button(self.window, text="Играть", command=self.play, fg="green").grid(row=3, column=3)
 
-        Label(self.window, text="Зверек", width=25, height=5).grid(row=0, column=1)
+        Label(self.window, text="gek", width=25, height=5).grid(row=0, column=1)
 
 
     def tick(self):
