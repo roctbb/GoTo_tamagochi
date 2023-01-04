c570bd939f6ae16bacdaff349cc761dc4e638fa0
krekerysu <dimasidorovsciy@gmail.com>
2023-01-04 15:21:38 +0300

    Сделал картинки

--- src/gui/animal_window.py
+++ src/gui/animal_window.py
@@ -1,13 +1,15 @@
 from tkinter.ttk import *
 from tkinter import *
 from tokenize import Name
-
+from tkinter import *
+from PIL import Image, ImageTk
 from pkg_resources.extern import names
 
+from config import IMAGES
+from domain.common import asset_path
 from domain.game import Game
 
-number = 0
-
+Label = Label
 
 class AnimalWindow:
 
@@ -16,6 +18,13 @@ class AnimalWindow:
         self.window.title("Питомец")
 
         self.animal = animal
+
+        self.__images = {}
+        for state in ["normal", "bad"]:
+            image_path = asset_path(IMAGES[self.animal.image][state])
+            img = Image.open(image_path).resize((100, 100))
+            img_photo = ImageTk.PhotoImage(img)
+            self.__images[state] = img_photo
 
         self.health_var = IntVar()
         self.hunger_var = IntVar()
@@ -26,16 +35,20 @@ class AnimalWindow:
         print("Голод -", self.hunger_var.get(), ",", "Здоровье -", self.health_var.get(), ",", "Настроение -",
               self.mood_var.get())
 
-        Progressbar(self.window, variable=self.hunger_var, maximum=animal.pr_max_hunger).grid(row=1, column=2)
-        Button(self.window, text="Покормить", command=self.feed, fg="green").grid(row=1, column=3)
+        Progressbar(self.window, variable=self.hunger_var, maximum=animal.pr_max_hunger).grid(row=1, column=1)
+        Button(self.window, text="Кормить", command=self.feed, fg="green").grid(row=1, column=2)
 
-        Progressbar(self.window, variable=self.health_var, maximum=animal.pr_max_health).grid(row=2, column=2)
-        Button(self.window, text="Помыть", command=self.wash, fg="green").grid(row=2, column=3)
+        Progressbar(self.window, variable=self.health_var, maximum=animal.pr_max_health).grid(row=2, column=1)
+        Button(self.window, text="Помыть", command=self.wash, fg="green").grid(row=2, column=2)
 
-        Progressbar(self.window, variable=self.mood_var, maximum=animal.pr_max_mood).grid(row=3, column=2)
-        Button(self.window, text="Играть", command=self.play, fg="green").grid(row=3, column=3)
+        Progressbar(self.window, variable=self.mood_var, maximum=animal.pr_max_mood).grid(row=3, column=1)
+        Button(self.window, text="Играть", command=self.play, fg="green").grid(row=3, column=2)
 
-        Label(self.window, text=image_names(), width=25, height=5).grid(row=0, column=1)
+        Label(self.window, text=animal.name, width=25, height=5).grid(row=0, column=1)
+        image_label = Label(self.window, image=self.__images[self.animal.state])
+        image_label.grid(row=1, column=0, rowspan=3)
+
+        image_label.configure(image_label=self.animal.state)
 
     def tick(self):
         self.update()
@@ -44,6 +57,8 @@ class AnimalWindow:
         self.health_var.set(self.animal.health)
         self.hunger_var.set(self.animal.hunger)
         self.mood_var.set(self.animal.mood)
+
+
 
     def feed(self):
         print("feeding")
--- tests/test.py
+++ tests/test.py
@@ -1,45 +1,11 @@
-from tkinter.ttk import *
 from tkinter import *
-from tkinter import messagebox
+from PIL import Image, ImageTk
 
+img1 = ImageTk.PhotoImage(Image.open("face1.jpg"))
+img2 = ImageTk.PhotoImage(Image.open("face2.jpg"))
 
-# Функция для уменьшения жизни каждую секунду
-def check_life():
-    # узнаем, какая сейчас жизнь из прогресс бара
-    current_life = life.get()
+image_label = Label(self.window, image=img1)
+image_label.pack()
 
-    # уменьшаем значение на прогрессбаре
-    life.set(current_life - 1)
-
-    # если жизни мало - закрываем приложение
-    if current_life <= 1:
-        messagebox.showerror(title="Вы проиграли!", message="Зверек умер... :(")
-        quit()
-    # иначе запускаем функция еще через секунду
-    else:
-        window.after(1000, check_life)
-
-
-# функция для кормления
-def feed():
-    # устанавливаем жизнь на максимум
-    life.set(100)
-
-
-window = Tk()
-window.title("Питомец")
-
-# поскольку жизнь - числовая характеристика, вместо StringVar - IntVar
-life = IntVar()
-life.set(50)
-
-# добавляем прогресс-бар
-Progressbar(window, variable=life).pack(padx=25, pady=25)
-
-# добавляем кнопку для увеличения жизни
-Button(window, text="Покормить", command=feed).pack(padx=25, pady=25)
-
-# включаем таймер для уменьшения жизни через секунду
-window.after(1000, check_life)
-
-mainloop()
+# когда хотим поменять картинку - вызываем configure
+image_label.configure(image=img2)
