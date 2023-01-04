3ef0307a52f0e778983a7fefabe1af8498dbc860
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-01-04 12:50:39 +0300

    Добавил файлы, доделал словарь

--- src/config.py
+++ src/config.py
@@ -3,22 +3,26 @@ import os
 MEAN_ANIMAL_PARAM = 20
 ANIMAL_SPREAD = 5
 
-images = {
+IMAGES = {
     "1": {
-        "normal": f"images{os.sep}2Bnorm",
-        "bad": f"images{os.sep}2Bbad",
+        "normal": f"images{os.sep}2Bnorm.png",
+        "bad": f"images{os.sep}2Bbad.png",
     },
     "2": {
-        "normal": f"images{os.sep}2B2norm",
-        "bad": f"images{os.sep}2B2bad",
+        "normal": f"images{os.sep}2B2norm.png",
+        "bad": f"images{os.sep}2B2bad.png",
     },
     "3": {
-        "normal": f"images{os.sep}2B2norm",
-        "bad": f"images{os.sep}2B2bad",
+        "normal": f"images{os.sep}Lusinorm.png",
+        "bad": f"images{os.sep}Lusibad.png",
     },
-
-
-
-
+    "4": {
+        "normal": f"images{os.sep}02norm.png",
+        "bad": f"images{os.sep}02bad.png",
+    },
+    "5": {
+        "normal": f"images{os.sep}Speedwagonnorm.png",
+        "bad": f"images{os.sep}Speedwagonbad.png",
+    },
 
 }
