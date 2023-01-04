affb02666a15cf69e99bd928593d4a3b5421b488
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-01-04 12:43:18 +0300

    обновляю пикчи

--- src/config.py
+++ src/config.py
@@ -1,3 +1,24 @@
+import os
+
 MEAN_ANIMAL_PARAM = 20
 ANIMAL_SPREAD = 5
 
+images = {
+    "1": {
+        "normal": f"images{os.sep}2Bnorm",
+        "bad": f"images{os.sep}2Bbad",
+    },
+    "2": {
+        "normal": f"images{os.sep}2B2norm",
+        "bad": f"images{os.sep}2B2bad",
+    },
+    "3": {
+        "normal": f"images{os.sep}2B2norm",
+        "bad": f"images{os.sep}2B2bad",
+    },
+
+
+
+
+
+}
